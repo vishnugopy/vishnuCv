1de74e676ded7dd946d01bc22f80d51339f060ef
vishnugopy <vishnugoby0010@gmail.com>
2020-12-08 23:24:23 +0100

    😍

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
         if(entry.intersectionRatio > 0) {
             entry.target.classList.add('into-appear');
-            console.log("helo");
         }
         else {
             entry.target.classList.add('box');
@@ -17,21 +16,3 @@ observer = new IntersectionObserver((entries) => {
 boxes.forEach(boxes => {
     observer.observe(boxes)
 })
-
-
-
-
-// function scrollApear(){
-//     var box = document.querySelectorAll(".box");
-//     var introPostion = box.getBoundingClientRect().top;
-//     var screenPosition = window.innerHeight ;
-    
-    
-//             if(introPostion < screenPosition){
-//                 box.classList.add('into-appear');
-//                 console.log("hezllo");
-//             }
-    
-//     }
-    
-//     window.addEventListener('scroll',scrollApear);
