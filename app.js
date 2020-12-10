41d16038059172129e5c267a36ff734fcc09f296
vishnugopy <vishnugoby0010@gmail.com>
2020-12-10 09:42:44 +0100

    😍

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const boxes = document.querySelectorAll('.box');
 observer = new IntersectionObserver((entries) => {
 
     entries.forEach(entry => {
-        if(entry.intersectionRatio > 0) {
+        if (entry.intersectionRatio > 0) {
             entry.target.classList.add('into-appear');
         }
         else {
