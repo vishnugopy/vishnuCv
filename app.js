85e63ed3a8cc27b38c87990dab4c14d3f2a80f92
vishnugopy <vishnugoby0010@gmail.com>
2020-12-11 01:13:40 +0100

    😍

--- app.js
+++ app.js
@@ -16,3 +16,32 @@ observer = new IntersectionObserver((entries) => {
 boxes.forEach(boxes => {
     observer.observe(boxes)
 })
+
+
+const darkButton = document.getElementById('dark');
+const lightButton = document.getElementById('light');
+const body = document.body;
+
+
+// Apply the cached theme on reload
+
+const theme = localStorage.getItem('theme');
+const isSolar = localStorage.getItem('isSolar');
+
+if (theme) {
+  body.classList.add(theme);
+  isSolar && body.classList.add('solar');
+}
+
+// Button Event Handlers
+
+darkButton.onclick = () => {
+  body.classList.replace('light', 'dark');
+  localStorage.setItem('theme', 'dark');
+};
+
+lightButton.onclick = () => {
+  body.classList.replace('dark', 'light');
+
+  localStorage.setItem('theme', 'light');
+};
